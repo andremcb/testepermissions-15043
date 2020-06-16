d6631976f6ddf063aa19649ce5e6ef411a37de9a
André Machado <csantos.machado@gmail.com>
2020-06-16 18:24:48 +0000

    Update models

--- backend/testepermissions_15043/wsgi.py
+++ backend/testepermissions_15043/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testepermissions_15043.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testepermissions_15043.settings")
 
 application = get_wsgi_application()
